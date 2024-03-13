65c1d0ee7c2057120fb38301bf788389721b57f4
langnang <langnang.chen@outlook.com>
2024-03-13 19:08:26 +0800

    config

--- Config/config.php
+++ Config/config.php
@@ -1,25 +1,98 @@
 <?php
 
-return [
-    'name' => 'Blog',
-    'prefix' => 'blog',
-    'web' => [
-        'theme' => '',
-    ],
-    'admin' => [
-        'sidebar' => [
-            ["path" => "/blog", "title" => 'Blog', "icon" => "fas fa-layer-group", "slug" => "", "children" => [
-                ["path" => "/metas", "title" => "Metas",],
-                ["path" => "/contents", "title" => "Contents", "children" => [
-                    ["path" => "/insert", "title" => "Insert", "visible" => false,],
-                    // ["path" => "/update/{id}", "title" => "编辑", "visible" => false,],
-                    ["path" => "/{id}", "title" => "Detail", "visible" => false,],
-                ],],
-                ["path" => "/comments", "title" => "Comments",],
-                ["path" => "/links", "title" => "Links",],
-                ["path" => "/options", "title" => "Options",],
-                ["path" => "/config", "title" => "Config",],
-            ]],
-        ]
-    ],
-];
+return array (
+  'name' => 'Blog',
+  'prefix' => 'blog',
+  'web' => 
+  array (
+    'theme' => '',
+  ),
+  'admin' => 
+  array (
+    'sidebar' => 
+    array (
+      0 => 
+      array (
+        'path' => '/blog',
+        'title' => 'Blog',
+        'icon' => 'fas fa-layer-group',
+        'slug' => '',
+        'children' => 
+        array (
+          0 => 
+          array (
+            'path' => '/metas',
+            'title' => 'Metas',
+          ),
+          1 => 
+          array (
+            'path' => '/contents',
+            'title' => 'Contents',
+            'children' => 
+            array (
+              0 => 
+              array (
+                'path' => '/insert',
+                'title' => 'Insert',
+                'visible' => false,
+              ),
+              1 => 
+              array (
+                'path' => '/0',
+                'title' => 'Detail',
+                'visible' => false,
+              ),
+            ),
+          ),
+          2 => 
+          array (
+            'path' => '/comments',
+            'title' => 'Comments',
+          ),
+          3 => 
+          array (
+            'path' => '/links',
+            'title' => 'Links',
+          ),
+          4 => 
+          array (
+            'path' => '/options',
+            'title' => 'Options',
+          ),
+          5 => 
+          array (
+            'path' => '/config',
+            'title' => 'Config',
+          ),
+        ),
+      ),
+    ),
+  ),
+  'controllers' => 
+  array (
+    0 => '\\Modules\\Blog\\Http\\Controllers\\BlogController',
+  ),
+  'seeders' => 
+  array (
+    0 => '\\Modules\\Blog\\Database\\Seeders\\BlogDatabaseSeeder',
+  ),
+  'entities' => 
+  array (
+    0 => '\\Modules\\Blog\\Entities\\BlogComment',
+    1 => '\\Modules\\Blog\\Entities\\BlogContent',
+    2 => '\\Modules\\Blog\\Entities\\BlogField',
+    3 => '\\Modules\\Blog\\Entities\\BlogLink',
+    4 => '\\Modules\\Blog\\Entities\\BlogMeta',
+    5 => '\\Modules\\Blog\\Entities\\BlogRelationship',
+  ),
+  'factories' => 
+  array (
+  ),
+  'migrations' => 
+  array (
+  ),
+  'slug' => 'blog',
+  'title' => 'Admin',
+  'type' => 'project',
+  'description' => NULL,
+);
